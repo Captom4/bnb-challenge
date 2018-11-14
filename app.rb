bd237ed15dae62536288adfcd7dca6dcd7c8e421
Benjamin Kearns <benjaminkearns@Benjamins-MacBook-Pro.local>
2018-11-14 11:01:25 +0000

    created navbar and added to all pages

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra/base'
+
+class MakersBnB < Sinatra::Base
+
+  get '/' do
+    erb:'homepage'
+  end
+
+  get '/spaces' do
+    erb:'spaces'
+  end
+
+  get '/list-a-space' do
+    erb:'newspace'
+  end
+
+  get '/sign-in' do
+    erb:'signin'
+  end
+
+  get '/sign-up' do
+    erb:'signup'
+  end
+
+  run! if app_file == $0
+end
