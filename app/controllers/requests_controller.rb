99e22a02a5e6fa75a078a1325ab14546cc9ba212
Tom <you@example.com>
2018-11-18 17:42:26 +0000

    wrote more seeds for the seed file and allow user to see request that where made

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,13 +1,21 @@
+# frozen_string_literal: true
+
 class RequestsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @requests_by_user = current_user.requests
+    @request_for_user = current_user.spaces.map(&:requests)
+  end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_request
-    @space = Space.find(params[:id])
+
+  def set_post
+    @request = Request.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def request_params
-    params.require(:request).permit(:space_id, :).merge(user_id: current_user.id)
+    params.require(:request).permit(:space_id, :booked_date).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :spaces
+  resources :requests
   devise_for :users
   root 'visitors#index'
 end
